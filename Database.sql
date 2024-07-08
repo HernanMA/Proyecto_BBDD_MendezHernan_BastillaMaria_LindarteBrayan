004995ce06b1ac1f3c402bad005e21315e2c2609
Yesid20 <edgarcarloscamilo@gmail.com>
2024-07-08 08:30:52 -0500

    feat: :art: Se realizaron modificaciones en las tablas

--- Database.sql
+++ Database.sql
@@ -15,7 +15,8 @@ CREATE TABLE natural_park (
     declaration_date DATE NOT NULL,
     entity_id INT NOT NULL,
     FOREIGN KEY (entity_id) REFERENCES responsible_entity(entity_id),
-    entry_count INT NOT NULL
+    entry_count INT NOT NULL,
+    areas_num INT NOT NULL
 );
 
 CREATE TABLE accommodation (
@@ -25,7 +26,6 @@ CREATE TABLE accommodation (
     limit_cap INT NOT NULL,
     FOREIGN KEY (park_id) REFERENCES natural_park(park_id)
 );
-
 
 CREATE TABLE visitor (
     visitor_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -68,14 +68,34 @@ CREATE TABLE inventory (
     individual_count INT NOT NULL
 );
 
-CREATE TABLE research_project (
-    project_id INT PRIMARY KEY AUTO_INCREMENT,
-    budget DECIMAL(15, 2) NOT NULL,
-    execution_period VARCHAR(100) NOT NULL
+CREATE TABLE staff (
+    staff_id INT PRIMARY KEY AUTO_INCREMENT,
+    id_card VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    phone VARCHAR(100) NOT NULL,
+    mobile VARCHAR(50),
+    salary DECIMAL(10, 2) NOT NULL,
+    park_id INT NOT NULL,
+    FOREIGN KEY (park_id) REFERENCES natural_park(park_id),
+    type_staff ENUM('management_staff', 'surveillance_staff', 'conservation_staff', 'research_staff')
 );
 
-ALTER TABLE research_project ADD name VARCHAR (100);
+CREATE TABLE management_staff (
+    management_staff_id INT PRIMARY KEY AUTO_INCREMENT,
+    staff_id INT NOT NULL,
+    FOREIGN KEY (staff_id) REFERENCES staff(staff_id),
+    entry_id INT NOT NULL
+);
 
+CREATE TABLE entry (
+    entry_id INT PRIMARY KEY AUTO_INCREMENT,
+    entry_number INT NOT NULL,
+    management_staff_id INT NOT NULL,
+    FOREIGN KEY (management_staff_id) REFERENCES management_staff(management_staff_id),
+    park_id INT NOT NULL,
+    FOREIGN KEY (park_id) REFERENCES natural_park(park_id)
+);
 
 CREATE TABLE vehicle (
     vehicle_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -84,68 +104,44 @@ CREATE TABLE vehicle (
     type VARCHAR(50)
 );
 
-ALTER TABLE vehicle DROP COLUMN plate;
-
-CREATE TABLE personnel (
-    personnel_id INT PRIMARY KEY AUTO_INCREMENT,
-    id_card VARCHAR(100) NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    address VARCHAR(100) NOT NULL,
-    phone VARCHAR(100) NOT NULL,
-    mobile VARCHAR(50),
-    salary DECIMAL(10, 2) NOT NULL,
-    park_id INT NOT NULL,
-    FOREIGN KEY (park_id) REFERENCES natural_park(park_id)
-);
-
-RENAME TABLE personnel TO staff;
-
-CREATE TABLE management_personnel (
-    management_personnel_id INT PRIMARY KEY AUTO_INCREMENT,
-    personnel_id INT NOT NULL,
-    FOREIGN KEY (personnel_id) REFERENCES personnel(personnel_id),
-    entry_id INT NOT NULL
-);
-
-CREATE TABLE entry (
-    entry_id INT PRIMARY KEY AUTO_INCREMENT,
-    entry_number INT NOT NULL,
-    management_personnel_id INT NOT NULL,
-    FOREIGN KEY (management_personnel_id) REFERENCES management_personnel(management_personnel_id),
-    park_id INT NOT NULL,
-    FOREIGN KEY (park_id) REFERENCES natural_park(park_id)
-);
-
-CREATE TABLE surveillance_personnel (
-    surveillance_personnel_id INT PRIMARY KEY AUTO_INCREMENT,
-    personnel_id INT NOT NULL,
-    FOREIGN KEY (personnel_id) REFERENCES personnel(personnel_id),
+CREATE TABLE surveillance_staff (
+    surveillance_staff_id INT PRIMARY KEY AUTO_INCREMENT,
+    staff_id INT NOT NULL,
+    FOREIGN KEY (staff_id) REFERENCES staff(staff_id),
     area_id INT NOT NULL,
     FOREIGN KEY (area_id) REFERENCES areas(area_id),
     vehicle_id INT NOT NULL,
     FOREIGN KEY (vehicle_id) REFERENCES vehicle(vehicle_id)
 );
 
-CREATE TABLE conservation_personnel (
-    conservation_personnel_id INT PRIMARY KEY AUTO_INCREMENT,
-    personnel_id INT NOT NULL,
-    FOREIGN KEY (personnel_id) REFERENCES personnel(personnel_id),
+CREATE TABLE conservation_staff (
+    conservation_staff_id INT PRIMARY KEY AUTO_INCREMENT,
+    staff_id INT NOT NULL,
+    FOREIGN KEY (staff_id) REFERENCES staff(staff_id),
     area_id INT NOT NULL,
     FOREIGN KEY (area_id) REFERENCES areas(area_id),
     specialty VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE research_personnel (
-    research_personnel_id INT PRIMARY KEY AUTO_INCREMENT,
-    personnel_id INT NOT NULL,
-    FOREIGN KEY (personnel_id) REFERENCES personnel(personnel_id),
-    project_id INT NOT NULL,
-    FOREIGN KEY (project_id) REFERENCES research_project(project_id),
+CREATE TABLE research_staff (
+    research_staff_id INT PRIMARY KEY AUTO_INCREMENT,
+    staff_id INT NOT NULL,
+    FOREIGN KEY (staff_id) REFERENCES staff(staff_id),
     degree VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE research_project (
+    project_id INT PRIMARY KEY AUTO_INCREMENT,
+    budget DECIMAL(15, 2) NOT NULL,
+    execution_period VARCHAR(100) NOT NULL,
+    research_staff_id INT NOT NULL,
+    FOREIGN KEY (research_staff_id) REFERENCES research_staff(research_staff_id)
+);
 
 SHOW TABLES;
 
+SELECT *  FROM responsible_entity;
+
+SELECT * FROM natural_park;
 
 
